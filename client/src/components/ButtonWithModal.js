25ee37e1e05127e125a66518147121b71acfc600
Lester Tay <lester.tay@accenture.com>
2020-09-26 13:35:54 +0800

    change blue blanner in transaction

--- client/src/components/ButtonWithModal.js
+++ client/src/components/ButtonWithModal.js
@@ -1,164 +1,163 @@
 import React, { Fragment, useState } from "react";
-import {
-    Form,
-    InputNumber,
-    Input,
-    Button,
-    Modal,
-    Typography,
-    message,
-    Cascader,
-} from "antd";
+import { Form, InputNumber, Button, Modal, Typography, Cascader } from "antd";
 
-import { SendOutlined } from "@ant-design/icons";
+import { SendOutlined, DoubleLeftOutlined } from "@ant-design/icons";
 
 const { Text } = Typography;
 const options = [
-    {
-        value: "Nigel",
-        label: "Nigel",
-    },
-    {
-        value: "Anthony",
-        label: "Anthony",
-    },
-    {
-        value: "Lester",
-        label: "Lester",
-    },
-    {
-        value: "Chen Qiu",
-        label: "Chen Qiu",
-    },
-    {
-        value: "Guo Xiong",
-        label: "Guo Xiong",
-    },
+  {
+    value: "Nigel",
+    label: "Nigel",
+  },
+  {
+    value: "Anthony",
+    label: "Anthony",
+  },
+  {
+    value: "Lester",
+    label: "Lester",
+  },
+  {
+    value: "Chen Qiu",
+    label: "Chen Qiu",
+  },
+  {
+    value: "Guo Xiong",
+    label: "Guo Xiong",
+  },
 ];
 
 const NewTransactionForm = ({ visible, onNewTransaction, onCancel }) => {
-    const [form] = Form.useForm();
+  const [form] = Form.useForm();
 
-    function onChange(value, selectedOptions) {
-        console.log(value, selectedOptions);
-    }
+  function onChange(value, selectedOptions) {
+    console.log(value, selectedOptions);
+  }
 
-    function filter(inputValue, path) {
-        return path.some(
-            (option) =>
-                option.label.toLowerCase().indexOf(inputValue.toLowerCase()) >
-                -1
-        );
-    }
-
-    return (
-        <Modal
-            visible={visible}
-            title="New Transaction"
-            okText="Send"
-            cancelText="Cancel"
-            onCancel={onCancel}
-            onOk={() => {
-                form.validateFields()
-                    .then((values) => {
-                        form.resetFields();
-                        console.log("values", values);
-                    })
-                    .catch((info) => {
-                        console.log("Validate Failed:", info);
-                    });
-            }}
-        >
-            <Form
-                form={form}
-                layout="vertical"
-                name="form_in_modal"
-                initialValues={{
-                    modifier: "public",
-                }}
-            >
-                <Form.Item
-                    label="Amount"
-                    name="amount"
-                    rules={[
-                        {
-                            required: true,
-                            message: "Please input your amount!",
-                        },
-                    ]}
-                >
-                    <InputNumber
-                        defaultValue={0}
-                        formatter={(value) =>
-                            `$ ${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ",")
-                        }
-                        parser={(value) => value.replace(/\$\s?|(,*)/g, "")}
-                        type="amount"
-                        placeholder="Enter Amount"
-                    />
-                </Form.Item>
-                <Form.Item
-                    name="recepient"
-                    label="Recepient"
-                    rules={[
-                        {
-                            required: true,
-                            message: "Please select your recepient!",
-                        },
-                    ]}
-                >
-                    <Cascader
-                        name="recepient"
-                        options={options}
-                        onChange={onChange}
-                        placeholder="Please select"
-                        showSearch={{ filter }}
-                    />
-                </Form.Item>
-            </Form>
-        </Modal>
+  function filter(inputValue, path) {
+    return path.some(
+      (option) =>
+        option.label.toLowerCase().indexOf(inputValue.toLowerCase()) > -1
     );
+  }
+
+  return (
+    <Modal
+      visible={visible}
+      title="New Transaction"
+      okText="Send"
+      cancelText="Cancel"
+      onCancel={onCancel}
+      onOk={() => {
+        form
+          .validateFields()
+          .then((values) => {
+            form.resetFields();
+            console.log("values", values);
+          })
+          .catch((info) => {
+            console.log("Validate Failed:", info);
+          });
+      }}
+    >
+      <Form
+        form={form}
+        layout="vertical"
+        name="form_in_modal"
+        initialValues={{
+          modifier: "public",
+        }}
+      >
+        <Form.Item
+          label="Amount"
+          name="amount"
+          rules={[
+            {
+              required: true,
+              message: "Please input your amount!",
+            },
+          ]}
+        >
+          <InputNumber
+            defaultValue={0}
+            formatter={(value) =>
+              `$ ${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+            }
+            parser={(value) => value.replace(/\$\s?|(,*)/g, "")}
+            type="amount"
+            placeholder="Enter Amount"
+          />
+        </Form.Item>
+        <Form.Item
+          name="recepient"
+          label="Recepient"
+          rules={[
+            {
+              required: true,
+              message: "Please select your recepient!",
+            },
+          ]}
+        >
+          <Cascader
+            name="recepient"
+            options={options}
+            onChange={onChange}
+            placeholder="Please select"
+            showSearch={{ filter }}
+          />
+        </Form.Item>
+      </Form>
+    </Modal>
+  );
 };
 const NewTransactionButton = (props) => {
-    const [visible, setVisible] = useState(false);
+  const [visible, setVisible] = useState(false);
 
-    const onNewTransaction = (data) => {
-        console.log("hellodata, ", data);
-    };
-    return (
-        <Fragment>
-            {/* <Button
+  const onNewTransaction = (data) => {
+    console.log("hellodata, ", data);
+  };
+  return (
+    <Fragment>
+      <Button
         type="primary"
         style={{
           marginTop: "16px",
-          padding: "0 20px 0 20px",
+          padding: "0 15px 0 15px",
+          borderRadius: 6,
         }}
         onClick={() => {
           setVisible(true);
         }}
+        size="large"
+        icon={<SendOutlined style={{ fontSize: "11px" }} />}
       >
-        <strong>New Transaction</strong>
-      </Button> */}
+        Send
+      </Button>
+      <Button
+        type="primary"
+        style={{
+          marginTop: "16px",
+          padding: "0 15px 0 15px",
+          borderRadius: 6,
+          marginLeft: 10,
+        }}
+        onClick={() => {
+          setVisible(true);
+        }}
+        size="large"
+        icon={<DoubleLeftOutlined style={{ fontSize: "11px" }} />}
+      >
+        Request
+      </Button>
 
-            <Button
-                className="modal-button"
-                onClick={() => {
-                    setVisible(true);
-                }}
-            >
-                <SendOutlined className="text-white icon-large" />
-                <h1 className="text-white text-center text-padding-top">
-                    New Transaction
-                </h1>
-            </Button>
-            <NewTransactionForm
-                visible={visible}
-                onNewTransaction={onNewTransaction}
-                onCancel={() => {
-                    setVisible(false);
-                }}
-            />
-        </Fragment>
-    );
+      <NewTransactionForm
+        visible={visible}
+        onNewTransaction={onNewTransaction}
+        onCancel={() => {
+          setVisible(false);
+        }}
+      />
+    </Fragment>
+  );
 };
 export default NewTransactionButton;
--- client/src/components/Transaction.js
+++ client/src/components/Transaction.js
@@ -3,59 +3,78 @@ import { Button, Row, Col, Typography } from "antd";
 import PastTransactions from "./PastTransactions";
 import ButtonWithModal from "./ButtonWithModal";
 import { HistoryOutlined, SettingOutlined } from "@ant-design/icons";
-const { Title } = Typography;
+const { Title, Text } = Typography;
 const Transaction = () => (
-    <Row style={{ paddingTop: 20 }} className="background-light-grey">
-        <Col span={4}></Col>
-        <Col span={16}>
+  <Row style={{ paddingTop: 20 }} className="background-light-grey">
+    <Col span={4}></Col>
+    <Col span={16}>
+      {" "}
+      <Title style={{ color: "#120309" }} level={2}>
+        {" "}
+        Welcome Back, Zhi Wei
+      </Title>
+      <Row
+        style={{
+          width: "100%",
+          backgroundColor: "#2392cc",
+          minHeight: 250,
+          borderRadius: 14,
+          boxShadow: "-4px 3px 16px -4px rgba(35,146,204,0.86)",
+          marginVertical: 40,
+          marginTop: 30,
+        }}
+      >
+        <Row style={{ padding: 20, flex: 1, flexDirection: "column" }}>
+          <Text
+            style={{
+              position: "relative",
+              top: 10,
+              marginTop: 10,
+              color: "#F6F7EB",
+              fontSize: 28,
+            }}
+          >
+            Account Balance
+          </Text>
+          <Text
+            level={2}
+            style={{ marginTop: 0, color: "#F6F7EB", fontSize: 67 }}
+          >
             {" "}
-            <Title level={2}> Welcome Back, Zhi Wei</Title>
-            <Row
-                style={{
-                    width: "100%",
-                    backgroundColor: "#2392cc",
-                    minHeight: 200,
-                    borderRadius: 14,
-                    boxShadow:
-                        "box-shadow: -4px 3px 24px 0px rgba(28,128,187,0.41);",
-                }}
-            >
-                <Col
-                    md={8}
-                    sm={24}
-                    className="justify-center modal-button-container"
-                >
-                    <ButtonWithModal />
-                </Col>
-                <Col
-                    md={8}
-                    sm={24}
-                    className="justify-center cursor-pointer modal-button-container"
-                >
-                    <HistoryOutlined className="text-white icon-large" />
-                    <h1 className="text-white text-center text-padding-top">
-                        Past Transactions
-                    </h1>
-                </Col>
-                <Col
-                    md={8}
-                    sm={24}
-                    className="justify-center cursor-pointer modal-button-container"
-                >
-                    <SettingOutlined className="text-white icon-large" />
-                    <h1 className="text-white text-center text-padding-top">
-                        Settings
-                    </h1>
-                </Col>
-            </Row>
+            $1890.30{" "}
+          </Text>
+        </Row>
+
+        {/* <Col md={8} sm={24} className="justify-center modal-button-container">
         </Col>
-        <Col span={4}></Col>
-        <Col span={4}></Col>
-        <Col span={16}>
-            <PastTransactions />
+        <Col
+          md={8}
+          sm={24}
+          className="justify-center cursor-pointer modal-button-container"
+        >
+          <HistoryOutlined className="text-white icon-large" />
+          <h1 className="text-white text-center text-padding-top">
+            Past Transactions
+          </h1>
         </Col>
-        <Col span={4}></Col>
-    </Row>
+        <Col
+          md={8}
+          sm={24}
+          className="justify-center cursor-pointer modal-button-container"
+        >
+          <SettingOutlined className="text-white icon-large" />
+          <h1 className="text-white text-center text-padding-top">Settings</h1>
+        </Col> */}
+      </Row>
+      <ButtonWithModal />
+    </Col>
+    <Col span={4}></Col>
+    <Col span={4}></Col>
+    <Col span={16}>
+      <PastTransactions />
+    </Col>
+    <Col span={4}></Col>
+  </Row>
 );
 
 export default Transaction;
