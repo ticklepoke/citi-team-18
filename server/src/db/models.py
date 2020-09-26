d103cbec31107254c7bdde6e76413130ffdf7f89
zhiwei.chin@motional.com <chinzhiweiblank@gmail.com>
2020-09-26 11:12:46 +0800

    Add 2FA token

--- server/src/db/models.py
+++ server/src/db/models.py
@@ -4,12 +4,14 @@ from pydantic import BaseModel
 
 Base = declarative_base()
 
+
 class UserAccount(Base):
-    __tablename__ = 'user_accounts'
+    __tablename__ = "user_accounts"
 
     user_id = Column(Integer, primary_key=True)
     username = Column(String)
     password = Column(String)
+    mobile_number = Column(string)
 
     def __eq__(self, other):
         return (
@@ -17,20 +19,52 @@ class UserAccount(Base):
             and other.user_id == self.user_id
             and other.username == self.username
             and other.password == self.password
+            and other.mobile_number == self.mobile_number
         )
 
     def __repr__(self):
-        return f'UserAccount(id={self.user_id}, username={self.username}, password={self.password})'
+        return f"UserAccount(id={self.user_id}, username={self.username}, password={self.password}, mobile_number={self.mobile_number})"
+
+
+class User2FA(Base):
+    __tablename__ = "user_tokens"
+    username = Column(String, primary_key=True)
+    token = Column(String)
+
+    def __eq__(self, other):
+        return (
+            isinstance(other, User2FA)
+            and other.username == self.username
+            and other.token == self.token
+        )
+
+    def __repr__(self):
+        return f"UserAccount(username={self.username} and token={self.token})"
+
 
 class UserAccountDetails(BaseModel):
     username: str
     password: str
+    mobile_number: str
 
 
 class UserAccountResponse(UserAccountDetails):
     user_id: int
 
 
+class User2FARequest(BaseModel):
+    username: str
+
+
+class User2FAKey(BaseModel):
+    username: str
+    token: str
+
+
 class Token(BaseModel):
     access_token: str
     token_type: str
+
+
+class Token2FA(BaseModel):
+    token: str
--- server/src/db/orm.py
+++ server/src/db/orm.py
@@ -1,15 +1,18 @@
 from sqlalchemy.orm import sessionmaker, Session
 from sqlalchemy import create_engine, Integer, String, Column
 from typing import Optional
-from .models import UserAccount, UserAccountDetails
+from .models import UserAccount, UserAccountDetails, User2FARequest, User2FAKey
 from .hashing import encrypt_password
+from uuid import uuid4
+import random
 import os
 
-DB_URL = os.getenv('DB_URL', 'postgresql+pg8000://postgres:@localhost:5432')
+DB_URL = os.getenv("DB_URL", "postgresql+pg8000://postgres:@localhost:5432")
 
-engine = create_engine(DB_URL, client_encoding='utf8')
+engine = create_engine(DB_URL, client_encoding="utf8")
 
 LocalSession = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 
 def get_db_session():
     db = LocalSession()
@@ -18,18 +21,38 @@ def get_db_session():
     finally:
         db.close()
 
-def get_user_account(session: Session, model: UserAccount, username: str) -> Optional[str]:
+
+def get_user_account(session: Session, model: UserAccount, username: str):
     sess = session or LocalSession()
     result = sess.query(model).filter(model.username == username).first()
     if result is None:
         return None
     return result
 
-def create_user_account(session: Session, user_account: UserAccountDetails):
-    sess  = session or LocalSession()
-    hashed_password = encrypt_password(user_account.password)
-    user_account = UserAccount(username=username, password=password)
+
+def create_user_account(session: Session, user: UserAccountDetails):
+    sess = session or LocalSession()
+    hashed_password = encrypt_password(user.password)
+    user_account = UserAccount(username=user.username, password=hashed_password)
     sess.add(user_account)
     sess.commit()
     sess.refresh(user_account)
     return user_account
+
+
+def create_2fa_token(session: Session, user: User2FARequest):
+    sess = session or LocalSession()
+    random_token = " ".join([str(random.randint(0, 999)).zfill(3) for _ in range(2)])
+    user_2fa_entry = User2FAKey(user.username, random_token)
+    sess.add(user_account)
+    sess.commit()
+    sess.refresh(user_2fa_entry)
+    return user_2fa_entry
+
+
+def get_user_token(session: Session, username: str):
+    sess = session or LocalSession()
+    result = sess.query(User2FAKey).filter(User2FAKey.username == username).first()
+    if result is None:
+        return None
+    return result
