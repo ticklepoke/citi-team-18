962144855376c4d333fa05db158cd6caa84cb6a6
zhiwei.chin@motional.com <chinzhiweiblank@gmail.com>
2020-09-26 09:33:19 +0800

    Remove redundant code

--- server/src/main.py
+++ server/src/main.py
@@ -9,12 +9,3 @@ port = os.getenv("PORT")
 app = FastAPI()
 
 app.include_router(auth_router, prefix="/auth", tags=["auth"])
-
-#@app.middleware("http")
-#async def db_session_middleware(request: Request, call_next):
-#    try:
-#        request.state.db = LocalSession()
-#        response = await call_next(request)
-#    finally:
-#        request.state.db.close()
-#    return response
