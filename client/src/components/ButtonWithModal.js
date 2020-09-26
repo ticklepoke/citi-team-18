19022e798c44cde19c148e4440c8dc6d3154f743
Nigel <lee.nigel@hotmail.com>
2020-09-26 12:37:39 +0800

    home page

--- client/src/components/ButtonWithModal.js
+++ client/src/components/ButtonWithModal.js
@@ -1,47 +1,51 @@
 import React from "react";
 import { Modal, Button } from "antd";
+import { SendOutlined } from "@ant-design/icons";
 
 class ButtonWithModal extends React.Component {
-  state = { visible: false };
+    state = { visible: false };
 
-  showModal = () => {
-    this.setState({
-      visible: true,
-    });
-  };
+    showModal = () => {
+        this.setState({
+            visible: true,
+        });
+    };
 
-  handleOk = (e) => {
-    console.log(e);
-    this.setState({
-      visible: false,
-    });
-  };
+    handleOk = (e) => {
+        console.log(e);
+        this.setState({
+            visible: false,
+        });
+    };
 
-  handleCancel = (e) => {
-    console.log(e);
-    this.setState({
-      visible: false,
-    });
-  };
+    handleCancel = (e) => {
+        console.log(e);
+        this.setState({
+            visible: false,
+        });
+    };
 
-  render() {
-    return (
-      <>
-        <Button type="primary" onClick={this.showModal}>
-          New Transaction
-        </Button>
-        <Modal
-          title="Basic Modal"
-          visible={this.state.visible}
-          onOk={this.handleOk}
-          onCancel={this.handleCancel}
-        >
-          <p>Some contents...</p>
-          <p>Some contents...</p>
-          <p>Some contents...</p>
-        </Modal>
-      </>
-    );
-  }
+    render() {
+        return (
+            <>
+                <Button className="modal-button" onClick={this.showModal}>
+                    <SendOutlined className="text-white icon-large" />
+                    <h1 className="text-white text-center text-padding-top">
+                        New Transaction
+                    </h1>
+                </Button>
+                <Modal
+                    title="Basic Modal"
+                    visible={this.state.visible}
+                    onOk={this.handleOk}
+                    onCancel={this.handleCancel}
+                >
+                    <p>Some contents...</p>
+                    <p>Some contents...</p>
+                    <p>Some contents...</p>
+                </Modal>
+            </>
+        );
+    }
 }
 export default ButtonWithModal;
