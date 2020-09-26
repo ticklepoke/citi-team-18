5d4bd07f608c069801f58b18dd2ab1e9d95c5583
zhiwei.chin@motional.com <chinzhiweiblank@gmail.com>
2020-09-26 09:31:35 +0800

    Test pre-commit

--- server/src/auth.py
+++ server/src/auth.py
@@ -7,16 +7,12 @@ ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 
-def create_access_token(
-    data: dict, expires_delta: Optional[timedelta] = None
-) -> str:
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None) -> str:
     to_encode = data.copy()
     if expires_delta:
         expire = datetime.utcnow() + expires_delta
     else:
         expire = datetime.utcnow() + timedelta(minutes=15)
     to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(
-        to_encode, os.getenv("SECRET_KEY"), algorithm=ALGORITHM
-    )
+    encoded_jwt = jwt.encode(to_encode, os.getenv("SECRET_KEY"), algorithm=ALGORITHM)
     return encoded_jwt
