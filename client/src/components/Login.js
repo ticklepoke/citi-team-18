60351fcc337a02d4078e0ca82cc7abfc78938065
Nigel <lee.nigel@hotmail.com>
2020-09-26 15:53:13 +0800

    typo

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -157,7 +157,7 @@ const Login = () => {
                         rules={[
                             {
                                 required: true,
-                                message: "Please input your password!",
+                                message: "Please input your Password!",
                             },
                         ]}
                     >
